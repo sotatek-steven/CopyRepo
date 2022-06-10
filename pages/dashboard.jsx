b5c7223d4200ef5e50df50ee9225068ef126a868
Steven <MY_NAME@example.com>
2022-06-10 07:38:38 +0700

    feat: done layout

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,14 +1,25 @@
-import { Box, CardContent, Grid } from '@mui/material';
-import React from 'react';
+import { Box, Button, CardContent, Grid } from '@mui/material';
+import React, { useState } from 'react';
 import { Card } from '@mui/material';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import TabsBar from 'components/layout/TabsBar';
+import { TabContext, TabPanel } from '@mui/lab';
+import { styled } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
 
 const dashboard = () => {
+  const [value, setValue] = useState('dashboard');
+
+  const TabPanelCustom = styled(TabPanel)(({ theme }) => ({
+    ...theme.mixins.toolbar,
+    paddingBottom: 0,
+  }));
+
   return (
     <Grid container spacing={2} sx={{ padding: '20px 8px 0 24px' }}>
       <Grid item xs={2}>
         <Card>
-          <CardContent sx={{ height: '100vh' }}>
+          <CardContent sx={{ height: '100vh', padding: '5px 24px' }}>
             <Box
               sx={{
                 display: 'flex',
@@ -27,8 +38,27 @@ const dashboard = () => {
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={10}>
-        <Card></Card>
+      <Grid item xs={8}>
+        {/* <Card></Card> */}
+        <TabsBar setTab={setValue} tab={value} />
+        <TabContext value={value}>
+          <TabPanelCustom value="All">
+            {/* <Overview /> */}
+            <Box sx={{ fontSize: '14px', alignItems: 'center' }}>
+              Build Your first smart contract with Ethereum & Polygon with an exciting way.
+            </Box>
+          </TabPanelCustom>
+        </TabContext>
+      </Grid>
+      <Grid item xs={2}>
+        <Box sx={{ display: 'flex', alignItems: 'center', pt: 2 }}>
+          <SearchIcon />
+          <Box sx={{ pl: 3 }}>
+            <Button variant="contained" startIcon={<AddCircleIcon />}>
+              Creat
+            </Button>
+          </Box>
+        </Box>
       </Grid>
     </Grid>
   );
