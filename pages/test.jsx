873aab0ccff3b83bcd5a418beb8e7f1005954b8d
Steven <MY_NAME@example.com>
2022-06-22 14:05:17 +0700

    fix margin header

---
+++ pages/test.jsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/PageLayout';
+import React from 'react';
+
+const test = () => {
+  return <div>language</div>;
+};
+
+// test.PageLayout = Layout;
+
+export default test;
