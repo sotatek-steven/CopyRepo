5318e29cb89a201b0dc8177a77e19c63da5701db
Sotatek-VuongHoang <vuong.hoang@sotatek.com>
2022-07-01 17:18:56 +0700

    feat: handle showing and hiding close icon feat: clear value input when click close icon

--- components/Library/SearchLibrary.jsx
+++ components/Library/SearchLibrary.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { styled } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Container = styled('div')(({ theme }) => ({
   background: theme.palette.background.light,
@@ -21,11 +22,32 @@ const Input = styled('input')(({ theme }) => ({
   width: '100%',
 }));
 
+const IconWrapper = styled('div')(({ hidden }) => ({
+  display: hidden ? 'hidden!imported' : 'flex',
+  alignItems: 'center',
+  cursor: 'pointer',
+}));
+
 const SearchLibrary = () => {
+  const [value, setValue] = useState('');
+  const [closeIconHidden, setCloseIconHidden] = useState(true);
+
+  useEffect(() => {
+    setCloseIconHidden(!value);
+  }, [value]);
+
+  const handleChange = (e) => {
+    const currentValue = e.target.value;
+    setValue(currentValue.trim());
+  };
+
   return (
     <Container>
       <SearchIcon style={{ fontSize: 22 }} />
-      <Input placeholder="Find library" />
+      <Input placeholder="Find library" value={value} onChange={handleChange} />
+      <IconWrapper hidden={closeIconHidden} onClick={() => setValue('')}>
+        <CloseIcon />
+      </IconWrapper>
     </Container>
   );
 };
