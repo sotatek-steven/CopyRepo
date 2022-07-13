a29db0c9170d9f1a5df889cb897a5197b7a12582
Sotatek-VuongHoang <vuong.hoang@sotatek.com>
2022-07-13 14:50:39 +0700

    feat-73: - feat: show new mapping item - feat: create SCOPE select

--- components/MappingTabPanel/index.jsx
+++ components/MappingTabPanel/index.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PrimaryButton } from '../ButtonStyle';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import styled from '@emotion/styled';
+import MappingItem from './MappingItem';
 
 const Container = styled('div')({
   padding: '30px 70px',
 });
 const MappingTabPanel = () => {
+  const [mappingItems, setMappingItems] = useState([]);
+  const handleClick = () => {
+    const newMappingItem = {
+      scope: '',
+      variableName: '',
+      mapTOFunction: [],
+    };
+
+    const _mappingItems = [...mappingItems];
+    _mappingItems.push(newMappingItem);
+
+    setMappingItems(_mappingItems);
+  };
+
   return (
     <Container>
-      <PrimaryButton width={123}>
+      {mappingItems?.map((mappingItem, index) => {
+        return <MappingItem key={index} />;
+      })}
+      <PrimaryButton width={123} onClick={handleClick}>
         <AddCircleOutlineIcon style={{ fontSize: 18 }} />
         <span>Create New</span>
       </PrimaryButton>
