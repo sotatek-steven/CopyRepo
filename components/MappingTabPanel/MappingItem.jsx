8de835510d77b70d0f4e79042803a839a4b18e9f
Sotatek-VuongHoang <vuong.hoang@sotatek.com>
2022-07-19 08:20:49 +0700

    feat-80 feat: remove mapping item

--- components/MappingTabPanel/MappingItem.jsx
+++ components/MappingTabPanel/MappingItem.jsx
@@ -1,14 +1,29 @@
-import { Grid } from '@mui/material';
+import { Grid, styled } from '@mui/material';
 import React from 'react';
 import KeyValueField from './KeyValueField';
 import MapToFunctionField from './MapToFunctionField';
 import ScopeField from './ScopeField';
 import VariableNameField from './VariableNameField';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+
+const RemoveButton = styled(RemoveCircleIcon)(({ theme }) => ({
+  color: theme.palette.primary.main,
+  fontSize: 22,
+  marginBottom: 12,
+  cursor: 'pointer',
+}));
 
 const MappingItem = (props) => {
+  const { removeItem, id } = props;
+
+  const handleClick = () => {
+    if (!removeItem) return;
+    removeItem(id);
+  };
+
   return (
     <div style={{ marginBottom: 60 }}>
-      <Grid container spacing={2}>
+      <Grid container spacing={4}>
         <Grid item xs={3}>
           <ScopeField {...props} />
         </Grid>
@@ -18,8 +33,10 @@ const MappingItem = (props) => {
         <Grid item xs={3}>
           <MapToFunctionField {...props} />
         </Grid>
+        <Grid item xs={3} style={{ display: 'flex', alignItems: 'end' }}>
+          <RemoveButton onClick={handleClick} />
+        </Grid>
       </Grid>
-
       <KeyValueField {...props} />
     </div>
   );
