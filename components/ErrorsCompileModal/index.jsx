b694bd8d07bf2db9087da545779ce5067c38d4d0
Sotatek-VuongHoang <vuong.hoang@sotatek.com>
2022-08-04 16:16:56 +0700

    feat-210 feat: display data of errors form

--- components/ErrorsCompileModal/index.jsx
+++ components/ErrorsCompileModal/index.jsx
@@ -1,5 +1,5 @@
 import { Modal, styled } from '@mui/material';
-import React, { useState } from 'react';
+import React from 'react';
 import Scrollbars from 'react-custom-scrollbars';
 import { ModalHeader } from '../atom/Modal';
 import { ModalBox } from '../atom/ModalBox';
@@ -7,20 +7,21 @@ import ErrorItem from './ErrorItem';
 
 const ModalBody = styled('div')({
   padding: '20px 30px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 20,
 });
 
-const ErrorsCompileModal = () => {
-  const [open, setOpen] = useState(true);
-  const handleClose = () => {
-    setOpen(false);
-  };
+const ErrorsCompileModal = ({ open, onClose, errors }) => {
   return (
-    <Modal open={open} onClose={handleClose}>
+    <Modal open={open} onClose={onClose}>
       <ModalBox width={850}>
-        <ModalHeader type="warning" title={`Errors (5)`} onClose={handleClose} />
+        <ModalHeader type="warning" title={`Errors (${errors.length})`} onClose={onClose} />
         <Scrollbars autoHeightMax={460} autoHeight>
           <ModalBody>
-            <ErrorItem />
+            {errors.map((item) => {
+              return <ErrorItem key={item.id} data={item} />;
+            })}
           </ModalBody>
         </Scrollbars>
       </ModalBox>
